cfa4c3169b19621283663deb9d5cde9b3152c875
Liangmu Zhu <nilaoyezhulmu@gmail.com>
2018-12-10 15:55:33 +1100

    Create ball_grid.py

---
+++ IntroPython/Practices/Week6/ball_grid.py
@@ -0,0 +1,24 @@
+# Ball grid solution
+
+###################################################
+# Student should enter code below
+
+import simplegui
+
+BALL_RADIUS = 20
+GRID_SIZE = 10
+WIDTH = 2 * GRID_SIZE * BALL_RADIUS
+HEIGHT = 2 * GRID_SIZE * BALL_RADIUS
+
+# define draw
+def draw(canvas):
+    for i in range(GRID_SIZE):
+        for j in range(GRID_SIZE):
+            canvas.draw_circle([(2 * i + 1) * BALL_RADIUS, (2 * j + 1) * BALL_RADIUS], BALL_RADIUS, 1, "WHITE")
+                      
+# create frame and register handlers
+frame = simplegui.create_frame("Ball grid", WIDTH, HEIGHT)
+frame.set_draw_handler(draw)
+
+# start frame
+frame.start()
